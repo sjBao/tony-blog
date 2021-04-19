cfa52bbe417766225f88dc49bb4e16befc0017b7
= <=>
2021-04-19 11:58:29 -0400

    Fix overlay width

---
+++ src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/Layout/layout';
+
+const BlogPage: React.FunctionComponent = () => {
+  return (
+    <Layout>
+      Blog Page
+    </Layout>
+  );
+};
+
+export default BlogPage;
